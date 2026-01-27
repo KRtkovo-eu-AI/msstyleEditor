6f87ee5dd2a8b0b54d034f3530fe235eac055a6b
Jakob <jjk@outlook.at>
2026-01-27 20:25:21 +0100

    avoid showing color names instead of the RGB triplet (#185)

--- msstyleEditorSharp/PropView/TypeConverters.cs
+++ msstyleEditorSharp/PropView/TypeConverters.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
 using System.Drawing;
 using System.Globalization;
+using System.Reflection;
 
 
 namespace msstyleEditor.PropView
@@ -89,8 +91,8 @@ namespace msstyleEditor.PropView
         {
             if (value is string s)
             {
-		        var found = -1;
-		        Int32.TryParse(s.Substring(0, s.IndexOf(" - ")), out found);
+                var found = -1;
+                Int32.TryParse(s.Substring(0, s.IndexOf(" - ")), out found);
                 foreach (var kvp in m_enumInfo)
                 {
                     if (kvp.Key == found) return kvp.Key;
@@ -105,7 +107,7 @@ namespace msstyleEditor.PropView
             if (value is int i)
             {
                 string result;
-                if(m_enumInfo.TryGetValue(i, out result))
+                if (m_enumInfo.TryGetValue(i, out result))
                 {
                     return $"{i} - {result}";
                 }
@@ -140,6 +142,92 @@ namespace msstyleEditor.PropView
         public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
         {
             return false;
+        }
+
+        // Like the original, but without "known", "named" or "system" colors.
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == null)
+            {
+                throw new ArgumentNullException("destinationType");
+            }
+
+            if (value is Color)
+            {
+                if (destinationType == typeof(string))
+                {
+                    Color color = (Color)value;
+                    if (color == Color.Empty)
+                    {
+                        return string.Empty;
+                    }
+
+                    if (culture == null)
+                    {
+                        culture = CultureInfo.CurrentCulture;
+                    }
+
+                    string separator = culture.TextInfo.ListSeparator + " ";
+                    TypeConverter converter = System.ComponentModel.TypeDescriptor.GetConverter(typeof(int));
+                    int num = 0;
+                    string[] array;
+                    if (color.A < byte.MaxValue)
+                    {
+                        array = new string[4];
+                        array[num++] = converter.ConvertToString(context, culture, color.A);
+                    }
+                    else
+                    {
+                        array = new string[3];
+                    }
+
+                    array[num++] = converter.ConvertToString(context, culture, color.R);
+                    array[num++] = converter.ConvertToString(context, culture, color.G);
+                    array[num++] = converter.ConvertToString(context, culture, color.B);
+                    return string.Join(separator, array);
+                }
+
+                if (destinationType == typeof(InstanceDescriptor))
+                {
+                    MemberInfo memberInfo = null;
+                    object[] arguments = null;
+                    Color color = (Color)value;
+                    if (color.IsEmpty)
+                    {
+                        memberInfo = typeof(Color).GetField("Empty");
+                    }
+                    else if (color.A != byte.MaxValue)
+                    {
+                        memberInfo = typeof(Color).GetMethod("FromArgb", new Type[4]
+                        {
+                            typeof(int),
+                            typeof(int),
+                            typeof(int),
+                            typeof(int)
+                        });
+                        arguments = new object[4] { color.A, color.R, color.G, color.B };
+                    }
+                    else
+                    {
+                        memberInfo = typeof(Color).GetMethod("FromArgb", new Type[3]
+                        {
+                            typeof(int),
+                            typeof(int),
+                            typeof(int)
+                        });
+                        arguments = new object[3] { color.R, color.G, color.B };
+                    }
+
+                    if (memberInfo != null)
+                    {
+                        return new InstanceDescriptor(memberInfo, arguments);
+                    }
+
+                    return null;
+                }
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
         }
     }
 }
