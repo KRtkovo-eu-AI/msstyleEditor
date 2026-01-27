0f667ffa03974e7402510218e5515725ea5b3a83
Jakob <jjk@outlook.at>
2026-01-27 21:18:33 +0100

    increment version number for release

--- libmsstyle/Properties/AssemblyInfo.cs
+++ libmsstyle/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("libmsstyle")]
-[assembly: AssemblyCopyright("Copyright © Jakob K. 2015-2024")]
+[assembly: AssemblyCopyright("Copyright © Jakob K. 2015-2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- msstyleEditorSharp/Properties/AssemblyInfo.cs
+++ msstyleEditorSharp/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("msstyleEditor")]
-[assembly: AssemblyCopyright("Copyright © Jakob K. 2015-2024")]
+[assembly: AssemblyCopyright("Copyright © Jakob K. 2015-2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("752c3bd1-8014-4d07-a198-823d0c394736")]
 
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
